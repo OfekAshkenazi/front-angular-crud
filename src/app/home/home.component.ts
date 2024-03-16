c7e61a3e9a34b604633ead3219bd3960a3ed1ef5
ofek ashkenazi <ofeka18@gmail.com>
2024-03-16 23:30:52 +0200

    start some traing to look for the new syntex

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  array = [1, 2, 3, 4, 56, 8];
+  myString = 'hellllllllo';
+  onClickMain() {
+    alert('wowowo angular');
+  }
 }
