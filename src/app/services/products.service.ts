be75812a3b64033802483e5623e7d900af146244
ofek ashkenazi <ofeka18@gmail.com>
2024-03-17 16:24:58 +0200

    added get function from my server to be access all around the app. then implant get function on the protucet service itself, working on interface of product and pagination

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  constructor(private apiservice: ApiService) {}
 
-  constructor() { }
+  getProducts(url: string, params: any): Observable<any> {
+    return this.apiservice.get(url, params);
+  }
 }
