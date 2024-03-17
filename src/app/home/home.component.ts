a02758bfa02d904705faf58ccb51bbb5691d6a8c
ofek ashkenazi <ofeka18@gmail.com>
2024-03-17 16:01:04 +0200

    CLEAR THE HOME CMP AND START WORKING ON SERVICES

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  array = [1, 2, 3, 4, 56, 8];
-  myString = 'hellllllllo';
-  onClickMain() {
-    alert('wowowo angular');
-  }
+  
 }
