4e31a93af172dfe0c13584760869e3f9ee0e5b8a
ofek ashkenazi <ofeka18@gmail.com>
2024-03-20 15:36:22 +0200

    added icons to angular.json and added confirmetion service from primeng for handling my delete function

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -3,15 +3,27 @@ import { Product } from './../../../types';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [RatingModule, FormsModule, ButtonModule],
+  imports: [
+    RatingModule,
+    FormsModule,
+    ButtonModule,
+    ToastModule,
+    ConfirmPopupModule,
+  ],
+  providers: [ConfirmationService],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
 })
 export class ProductComponent {
+  constructor(private ConfirmationService: ConfirmationService) {}
+
   @Input() product!: Product;
   @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
   @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
@@ -19,6 +31,15 @@ export class ProductComponent {
   editProduct() {
     this.edit.emit(this.product);
   }
+  confirmDelete() {
+    this.ConfirmationService.confirm({
+      message: 'Are you sure that you want to delete this product?',
+      accept: () => {
+        this.deleteProduct()
+      },
+    });
+  }
+
   deleteProduct() {
     this.delete.emit(this.product);
   }
